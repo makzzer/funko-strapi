0a8b38544bfc89fb244549467968770d585bc819
Maximiliano <makzofx@gmail.com>
2023-12-18 04:31:13 -0300

    linda card detalle producto

--- src/pages/DetallesProducto.jsx
+++ src/pages/DetallesProducto.jsx
@@ -26,7 +26,7 @@ const DetallesProducto = () => {
             "CATEGORIA_POR_DEFECTO",
           img:
             "http://localhost:1337" +
-            item.attributes.imagen.data.attributes.formats.small.url,
+            item.attributes.imagen.data.attributes.url,
           precio: item.attributes.precio,
           cuotas: `${item.attributes.cuotas} cuotas sin interés`,
           tag1: item.attributes.tag1,
@@ -53,12 +53,16 @@ const DetallesProducto = () => {
       });
   }, [id]);
 
-  console.log("esta es la longitud de los productos que trae la api:" + productos.length);
+  console.log(
+    "esta es la longitud de los productos que trae la api:" + productos.length
+  );
   console.log("este es el iddddddddddddd" + id);
 
   useEffect(() => {
     // Filtrar el producto por id después de obtener todos los datos adaptados
-    const productoFiltrado = productos.find((producto) => producto.id.toString() === id);
+    const productoFiltrado = productos.find(
+      (producto) => producto.id.toString() === id
+    );
     console.log("el id es+" + id);
 
     // Si se encuentra el producto, almacenarlo en el estado productoSeleccionado
@@ -67,29 +71,61 @@ const DetallesProducto = () => {
     }
   }, [productos, id]);
 
-  console.log("este es el producto seleccionado ", productoSeleccionado ? productoSeleccionado.title : "");
+  console.log(
+    "este es el producto seleccionado ",
+    productoSeleccionado ? productoSeleccionado.title : ""
+  );
 
   return (
     <>
       <div className="transition-all duration-500 md:max-w-6xl max-w-3xl px-4 md:px-0 mx-auto flex flex-col items-center container mt-10 md:mt-20">
-        <div className="bg-gray-100 mt-20 mb-20 rounded-lg overflow-hidden shadow-lg p-6">
-          <img
-            src={productoSeleccionado?.img} // Asegúrate de tener la propiedad img en tu objeto productoSeleccionado
-            alt={productoSeleccionado?.title}
-            className="w-full h-auto"
-          />
-          <h2 className="text-xl font-bold mt-4">{productoSeleccionado?.title}</h2>
-          <p className="text-gray-600">{productoSeleccionado?.description}</p>
-          <div className="flex justify-between mt-4">
-            <span className="text-blue-500 text-transform:uppercase">{productoSeleccionado?.precio}</span>
-            <button
-              className="bg-red-800 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-              onClick={volverPT}
-            >
-              Volver
-            </button>
+        {/*div para toda la card*/}
+        <div className="bg-gray-100 mt-20 gap-5 mb-20 rounded-lg overflow-hidden flex flex-col m-1 md:flex-row shadow-lg p-6">
+          {/*div para imagen en izquierda*/}
+          <div className="md:flex-none rounded-lg ">
+            {" "}
+            <img
+              src={productoSeleccionado?.img}
+              alt={productoSeleccionado?.title}
+              className="w-600 h-600 bg-w rounded-lg md:max-w-[600px] md:max-h-[600px]"
+            />
           </div>
+
+          {/*div para titulo,precio y descripción en izquierda*/}
+          <div className="flex flex-col md:mt-16 text-center">
+            {" "}
+            <h2 className="text-5xl md:text-6xl font-bold mt-2 md:mt-4 mb-6">
+              {productoSeleccionado?.title}
+            </h2>
+            <span className="text-red-600 mb-6 text-5xl  font-semibold text-transform:uppercase">
+              $ {productoSeleccionado?.precio}
+            </span>
+            <p className="text-gray-600 text-justify">
+              {productoSeleccionado?.description}
+            </p>
+
+                    
+        <div className="mx-auto md:mt-8 mt-6 items-end flex">
+              <button
+                className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+                onClick={volverPT}
+              >
+                Comprar
+              </button>
+            </div>
+
+
+
+          </div>
+
+
+
+
         </div>
+
+
+
+
       </div>
     </>
   );
