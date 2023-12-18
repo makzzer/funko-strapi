1df33c5d415a125b457101b9782b6986b181a962
Maximiliano <makzofx@gmail.com>
2023-12-18 04:33:20 -0300

    card ver producto v2

--- src/pages/DetallesProducto.jsx
+++ src/pages/DetallesProducto.jsx
@@ -110,7 +110,7 @@ const DetallesProducto = () => {
                 className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
                 onClick={volverPT}
               >
-                Comprar
+                Volver
               </button>
             </div>
 
