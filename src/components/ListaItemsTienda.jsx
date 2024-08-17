aef213b1febf2b1f19222654689f34b912e329f8
makzzer <makzofx@gmail.com>
2024-08-17 02:47:08 -0300

    ok sin errores

--- src/components/ListaItemsTienda.jsx
+++ src/components/ListaItemsTienda.jsx
@@ -1,32 +1,19 @@
 import ItemTienda from "./ItemTienda";
 
 const ListaItemsTienda = ({ elementosLand }) => {
-
-{/*comento estas clases en el div arriba de <ItemTienda    md:py-1 max-w-6xl mx-auto md:flex items-center container justify-between  md:px-10 px-2*/}
-
-
   return (
-    <>
-    <ul className="flex flex-col  px-10 mx-auto content-center justify-center">
-    {elementosLand.map((elem) => (
-        
-        <>
-        <li key={elem.id}>
+    <ul className="flex flex-col px-10 mx-auto content-center justify-center">
+      {elementosLand.map((elem) => (
+        <li key={elem.id}> {/* Asegúrate de que cada li tenga un key único */}
           <ItemTienda
             title={elem.title}
             description={elem.description}
             img={elem.img}
             id={elem.id}
           />
-          </li>
-        </>
+        </li>
       ))}
-
-
-
     </ul>
-
-    </>
   );
 };
 
